1000a030ae776cd33cb75c11a1cdb9a5c23637d8
Jasper-code-web <1156657702@qq.com>
2022-08-10 16:41:44 +0800

    let变量声明

--- ES6/1.let and const/index.js
+++ ES6/1.let and const/index.js
@@ -1,3 +1,12 @@
+/*
+ * @Author: Jasper-code-web 1156657702@qq.com
+ * @Date: 2022-08-10 10:05:19
+ * @LastEditors: Jasper-code-web 1156657702@qq.com
+ * @LastEditTime: 2022-08-10 11:31:21
+ * @FilePath: \code\code\ES6\1.let and const\index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+// "use strict"
 var temporal = 'jas'
 var a = 1
 if(true) {
@@ -6,6 +15,32 @@ if(true) {
     let temporal = 'Jasper'
 }
 
-console.log(typeof undeclared_variable)
-console.log(typeof x) //Uncaught ReferenceError: Cannot access 'x' before initialization
+console.log(typeof undeclared_variable) //undefined
+// console.log(typeof x) //Uncaught ReferenceError: Cannot access 'x' before initialization
 let x
+
+//IIFE Immediately Invoked Function Expression
+(function () {
+    // var tmp
+}())
+
+function f() {console.log('I am outside')}
+(function () {
+    console.log(f) //undefined
+    if(false) {
+        function f() {console.log('I an inside')}
+        console.log('a')
+    }
+    // f() //Uncaught TypeError: f is not a function
+}())
+
+
+// if(true) let x = 1 //error
+if(true) {
+    let x = 1
+}
+
+if(true) {
+    function f1() {}
+}
+// if(true) var t1 = 1
--- ES6/1.let and const/note.md
+++ ES6/1.let and const/note.md
@@ -22,11 +22,54 @@ ES6规定，在区块中一旦使用let和const声明了变量，那么相应的
 
 当然，这就导致了，在区块内声明了与区块外同名的变量也不会报错。
 
+```javascript
+console.log(typeof undeclared_variable) //undefined
+console.log(typeof x) //Uncaught ReferenceError: Cannot access 'x' before initialization
+let x
+```
 
+typeof在判断一个从未声明的变量时会返回undefined，不会报错。但是判断一个暂时未声明，但在之后使用let或const声明了该变量，就会报错。
 
+### 3.不允许重复声明
 
+### 4.块级作用域
 
+ES5只有函数作用域和全局作用域。ES5中for循环的问题就是因为for循环中定义的变量为全局作用域的。
 
+```javascript
+//IIFE Immediately Invoked Function Expression
+(function () {
+    var tmp
+}())
+```
+
+有了块级作用域，IIFE就没有存在的必要
+
+> ES6标准规定，函数的声明调用规则和let一样。也就是在块级作用域内声明，对外部没有影响。
+>
+> 但是在浏览器实现中函数的声明调用规则与var一样。也就是有变量提升，也没有块级作用域。
+>
+>
+> ```javascript
+> (function () {
+>  console.log(f) //undefined
+>  if(false) {
+>      function f() {console.log('I an inside')}
+>      console.log('a')
+>  }
+>  f() //Uncaught TypeError: f is not a function
+> }())
+> ```
+>
+
+>ES6的块级作用域必须有大括号。如果没有，js引擎就会认为不存在块级作用域
+>
+>```javascript
+>if(true) let x = 1 //error
+>if(true) {
+>    let x = 1
+>}
+>```
 
 
 
