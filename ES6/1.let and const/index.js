ed481557eb3aaf6328043c01a0c72e27f38665f6
Jasper-code-web <1156657702@qq.com>
2022-08-11 11:43:02 +0800

    variable declaration complete

--- ES6/1.let and const/index.js
+++ ES6/1.let and const/index.js
@@ -2,7 +2,7 @@
  * @Author: Jasper-code-web 1156657702@qq.com
  * @Date: 2022-08-10 10:05:19
  * @LastEditors: Jasper-code-web 1156657702@qq.com
- * @LastEditTime: 2022-08-10 11:31:21
+ * @LastEditTime: 2022-08-11 11:07:00
  * @FilePath: \code\code\ES6\1.let and const\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -44,3 +44,5 @@ if(true) {
     function f1() {}
 }
 // if(true) var t1 = 1
+
+// const PI
